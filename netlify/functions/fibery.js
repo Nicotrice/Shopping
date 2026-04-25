81823951210ba049d2c3faa33ca78beabc957a72
Nicotrice <77812105+Nicotrice@users.noreply.github.com>
2026-04-25 21:19:02 +0100

    Add files via upload

--- netlify/functions/fibery.js
+++ netlify/functions/fibery.js
@@ -1,35 +1,47 @@
+const https = require('https');
+
 exports.handler = async function(event) {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
   const FIBERY_TOKEN = '48be8a5c.60d2672deb1f5ff1d80ce4a146101193077';
-  const FIBERY_URL = 'https://dudley-house.fibery.io/api/graphql/space/Shopping%20Log';
 
-  try {
-    const response = await fetch(FIBERY_URL, {
+  return new Promise((resolve) => {
+    const options = {
+      hostname: 'dudley-house.fibery.io',
+      path: '/api/graphql/space/Shopping%20Log',
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Token ${FIBERY_TOKEN}`
-      },
-      body: event.body
+        'Authorization': `Token ${FIBERY_TOKEN}`,
+        'Content-Length': Buffer.byteLength(event.body)
+      }
+    };
+
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: data
+        });
+      });
     });
 
-    const data = await response.json();
+    req.on('error', (e) => {
+      resolve({
+        statusCode: 500,
+        body: JSON.stringify({ error: e.message })
+      });
+    });
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify(data)
-    };
-  } catch(e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message })
-    };
-  }
+    req.write(event.body);
+    req.end();
+  });
 };
