16ae355cda8f4f2d58cdbafa9118aeb532f46949
Nicotrice <77812105+Nicotrice@users.noreply.github.com>
2026-04-25 21:17:52 +0100

    Add files via upload

--- netlify/functions/fibery.js
+++ netlify/functions/fibery.js
@@ -1,1 +1,35 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
 
+  const FIBERY_TOKEN = '48be8a5c.60d2672deb1f5ff1d80ce4a146101193077';
+  const FIBERY_URL = 'https://dudley-house.fibery.io/api/graphql/space/Shopping%20Log';
+
+  try {
+    const response = await fetch(FIBERY_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${FIBERY_TOKEN}`
+      },
+      body: event.body
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch(e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
+  }
+};
